c9d712999453048cd24d92d36a0121f0cd09aee9
Donald <donald@DonalddeMacBook-Air.local>
2016-09-03 17:32:22 +0800

    dads

---
+++ collectionViewLayout/PPTCollectionViewCircleLayout.h
@@ -0,0 +1,13 @@
+//
+//  PPTCollectionViewCircleLayout.h
+//  collectionViewLayout
+//
+//  Created by Donald on 16/9/3.
+//  Copyright © 2016年 Susu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPTCollectionViewCircleLayout : UICollectionViewFlowLayout
+
+@end
---
+++ collectionViewLayout/PPTCollectionViewCircleLayout.m
@@ -0,0 +1,13 @@
+//
+//  PPTCollectionViewCircleLayout.m
+//  collectionViewLayout
+//
+//  Created by Donald on 16/9/3.
+//  Copyright © 2016年 Susu. All rights reserved.
+//
+
+#import "PPTCollectionViewCircleLayout.h"
+
+@implementation PPTCollectionViewCircleLayout
+
+@end
